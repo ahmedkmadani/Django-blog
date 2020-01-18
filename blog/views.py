77b809a98a970843c6e2155efb5e0975e93f81ef
ahmedkmadani <ahmedkamal543@gmail.com>
2020-01-18 17:24:01 +0000

    use date filter in home html get post from database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author':'Ahmedk',
-        'title' : 'Blog Post 1',
-        'content' : 'First Post content',
-        'date_posted' : 'Augest 27, 2020'
-    },
-       {
-        'author':'Ahmedk Madani',
-        'title' : 'Blog Post 2',
-        'content' : 'Second Post content',
-        'date_posted' : 'Augest 29, 2020'
-    }
-]
-
-# Create your views here.
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
